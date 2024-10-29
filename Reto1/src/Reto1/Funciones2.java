be0458a73de2858f97229b75b856b0ba44c37624
Alejillo478 <alejopv478@gmail.com>
2024-10-29 11:11:04 +0100

    Update Funciones2.java
    correción función dividir

--- Reto1/src/Reto1/Funciones2.java
+++ Reto1/src/Reto1/Funciones2.java
@@ -11,11 +11,7 @@ public class Funciones2 {
 
 	public static int dividir(int n1, int n2) {
 
-		if (n2 == 0)
-			return 0;
-		else
-			return (n1 / n2);
-	}
+		return (n1 / n2);
 
 	public static boolean numPositivo(int n1) {
 		if (n1 > 0)

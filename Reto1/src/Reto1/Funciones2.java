fe070dd60607a364e17c1de0e594509975f62531
DAM1 <DAM1@aula11-pc20>
2024-10-29 10:27:21 +0100

    añadido la funcion dividir, multiplicar ,numPositivo y Negativo

--- Reto1/src/Reto1/Funciones2.java
+++ Reto1/src/Reto1/Funciones2.java
@@ -1,10 +1,33 @@
 package Reto1;
 
+import java.util.Scanner;
+
 public class Funciones2 {
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int multiplico(int n1, int n2) {
+		return (n1 * n2);
 	}
 
+	public static int dividir(int n1, int n2) {
+
+		if (n2 == 0)
+			return 0;
+		else
+			return (n1 / n2);
+	}
+
+	public static boolean numPositivo(int n1) {
+		if (n1 > 0)
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean numNegativo(int n2) {
+		if (n2 < 0)
+			return true;
+		else
+			return false;
+	}
 }

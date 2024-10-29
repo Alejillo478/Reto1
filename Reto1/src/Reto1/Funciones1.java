fde193058b7c7a5b3c4f38fee3c1d29355f3ff00
Alejillo478 <alejopv478@gmail.com>
2024-10-29 10:06:17 +0100

    creado funciones 1

--- Reto1/src/Reto1/Funciones1.java
+++ Reto1/src/Reto1/Funciones1.java
@@ -1,49 +1,31 @@
 package Reto1;
 
-import java.util.Scanner;
-
 public class Funciones1 {
 
 	public static void main(String[] args) {
 
-		
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Introduce un número");
-		String s1 = sc.nextLine();
-		int n1 = Integer.parseInt(s1);
+	}
 
-		
+	public static int sumar(int a, int b) {
+		return (a + b);
+	}
 
-		boolean resultado=esImpar(n1);
-		System.out.println(resultado);
-		sc.close();
-		
+	public static int restar(int a, int b) {
+
+		return (a - b);
+
 	}
-	public static int sumar (int a,int b)
-	{
-		return (a+b);
-	}
-	public static int restar (int a,int b)
-	{
-		
-		return (a-b);
-		
-	}
-	
+
 	public static boolean esPar(int a)
-	
+
 	{
-		return a%2==0;
+		return a % 2 == 0;
 	}
 
-	public static boolean esImpar(int a)
-	{
-		
-		return a%2!=0;
-		
+	public static boolean esImpar(int a) {
+
+		return a % 2 != 0;
+
 	}
-	
-	
-	
-	
+
 }

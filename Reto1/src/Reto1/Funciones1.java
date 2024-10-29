99c394b2f162f44ec1fff89047d86d6301f6bffb
Alejillo478 <alejopv478@gmail.com>
2024-10-29 10:05:41 +0100

    funciones sumar, restar, esPar y esImpar

--- Reto1/src/Reto1/Funciones1.java
+++ Reto1/src/Reto1/Funciones1.java
@@ -1,10 +1,49 @@
 package Reto1;
 
+import java.util.Scanner;
+
 public class Funciones1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Introduce un número");
+		String s1 = sc.nextLine();
+		int n1 = Integer.parseInt(s1);
+
+		
+
+		boolean resultado=esImpar(n1);
+		System.out.println(resultado);
+		sc.close();
+		
+	}
+	public static int sumar (int a,int b)
+	{
+		return (a+b);
+	}
+	public static int restar (int a,int b)
+	{
+		
+		return (a-b);
+		
+	}
+	
+	public static boolean esPar(int a)
+	
+	{
+		return a%2==0;
 	}
 
+	public static boolean esImpar(int a)
+	{
+		
+		return a%2!=0;
+		
+	}
+	
+	
+	
+	
 }
